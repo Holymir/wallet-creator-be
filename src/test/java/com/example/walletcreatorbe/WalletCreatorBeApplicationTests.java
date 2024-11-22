1afcefe77cd861523bd80cf835aaa05fea8fb27f
Ventsi <ventsi@limechain.tech>
2024-11-22 07:08:42 +0200

    first commit

---
+++ src/test/java/com/example/walletcreatorbe/WalletCreatorBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.walletcreatorbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletCreatorBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
